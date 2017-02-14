c831af158f4d64b9d8fa2249125364dfc60f5c5d
tantalishe <tantalishe@gmail.com>
2017-02-14 11:23:03 +0300

    fix trackbars

--- svyat/detect_shapes_bars.py
+++ svyat/detect_shapes_bars.py
@@ -13,21 +13,18 @@ def nothing(x):
 
 cv2.namedWindow('bars',cv2.WINDOW_AUTOSIZE) 
 # create trackbars for color change
-cv2.createTrackbar('treshold_min', 'bars', 0, 255, nothing)
+cv2.createTrackbar('threshold_min', 'bars', 0, 255, nothing)
 cv2.createTrackbar('invert', 'bars', 0, 1, nothing)
 cv2.createTrackbar('blurtype', 'bars', 0, 3, nothing)
 cv2.createTrackbar('blur', 'bars', 0, 30, nothing)
 cv2.createTrackbar('size', 'bars', 480, 1280, nothing)
 # cv2.createTrackbar('mirror', 'bars', 0, 1, nothing)
-# cv2.createTrackbar('asfsfd', 'bars', 0, 1, nothing)
 
 while(True):
 	ret, image = cap.read()
-	# if cv2.getTrackbarPos('mirror','bars') == 1:
-	# 	image = cv2.flip(image,flipCode = 1)
 	invert = 0
 	#taking definitions of bars 
-	treshold_min = cv2.getTrackbarPos('treshold_min', 'bars')
+	threshold_min = cv2.getTrackbarPos('threshold_min', 'bars')
 	invert = cv2.getTrackbarPos('invert', 'bars')
 	blurtype = cv2.getTrackbarPos('blurtype', 'bars')
 	blur = cv2.getTrackbarPos('blur', 'bars')
@@ -36,20 +33,13 @@ while(True):
 	if size == 0:
 		size = 2
 	image = imutils.resize(image, width=size)
-
-	
-	if invert == 1:
-		image = cv2.bitwise_not(image)
-	else:
-		invert = 0
 	
 	ratio = 1
-	# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 	if blurtype == 0:
 		blurred = cv2.GaussianBlur(image, (blur, blur), 0)
 	elif blurtype == 1:
-		blurred = cv2.medianBlur(image,blur)
+		blurred = cv2.medianBlur(image, blur)
 	elif blurtype == 2:
 		blurred = cv2.blur(image,(blur, blur))
 	elif blurtype == 3:
@@ -57,17 +47,15 @@ while(True):
 
 	gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 
-	ret,thresh = cv2.threshold(gray, treshold_min, 255, cv2.THRESH_BINARY)
+	ret,thresh = cv2.threshold(gray, threshold_min, 255, cv2.THRESH_BINARY)
+
+	if invert == 1:
+		thresh = cv2.bitwise_not(thresh)
 
 	contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
 	contours = contours[0] if imutils.is_cv2() else contours[1]
 	sd = ShapeDetector()
-
-	if invert == 1:
-		image = cv2.bitwise_not(image)
-	else:
-		invert = 0
 
 	# loop over the contours
 	for contour in contours:
