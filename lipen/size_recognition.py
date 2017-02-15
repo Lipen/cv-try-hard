5f09b5731690a2429915b064f1a989cd78cc3d04
Lipen <lipen00@gmail.com>
2017-02-15 23:09:16 +0300

    Fix blur, add l (distance to object) trackbar, add some comments

--- lipen/size_recognition.py
+++ lipen/size_recognition.py
@@ -9,7 +9,8 @@ def do_nothing(_):
 
 
 def midpoint(ptA, ptB):
-    return (ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5
+    # return (ptA[0] + ptB[0]) / 2, (ptA[1] + ptB[1]) / 2
+    return (ptA + ptB) / 2
 
 
 def getWHImage(l, hfov=58, vfov=45, dfov=70):
@@ -22,6 +23,12 @@ def getWHImage(l, hfov=58, vfov=45, dfov=70):
     return width, height, diag
 
 
+def put_text(image, pos, *args, fmt='{:.0f}mm'):
+    cv2.putText(image, fmt.format(*args), (int(pos[0]), int(pos[1])),
+                cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7,
+                color=(255, 100, 0), thickness=2)
+
+
 def init():
     global cam
 
@@ -32,14 +39,15 @@ def init():
     cv2.namedWindow('MAIN')
     cv2.namedWindow('BARS')
 
-    cv2.createTrackbar('canny_low', 'BARS', 20, 100, do_nothing)
+    cv2.createTrackbar('sigma', 'BARS', 33, 100, do_nothing)
+    # cv2.createTrackbar('canny_low', 'BARS', 20, 100, do_nothing)
     # cv2.createTrackbar('canny_high', 'BARS', 150, 255, do_nothing)
+    cv2.createTrackbar('l', 'BARS', 50, 100, do_nothing)
     cv2.createTrackbar('blur', 'BARS', 3, 10, do_nothing)
-    cv2.createTrackbar('invert', 'BARS', 0, 1, do_nothing)
 
     cv2.moveWindow('MAIN', 55, 0)
     cv2.moveWindow('BARS', 60, 550)
-    cv2.resizeWindow('BARS', 1000, 100)
+    cv2.resizeWindow('BARS', 1000, 10)
 
 
 def main():
@@ -48,43 +56,49 @@ def main():
     while True:
         # Read frame
         _, image = cam.read()
-        # Flip
+
+        # Mirror
         image = cv2.flip(image, flipCode=1)
-        # Invert
-        if cv2.getTrackbarPos('invert', 'BARS'):
-            image = cv2.bitwise_not(image)
 
         # Grayscale
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
         # Blur
-        blur = cv2.getTrackbarPos('blur', 'BARS')
-        if blur % 2 == 0:
-            blur += 1
-        blurred = cv2.medianBlur(gray, 5)
+        blur = 1 + 2 * cv2.getTrackbarPos('blur', 'BARS')
+        blurred = cv2.medianBlur(gray, blur)
+
         # Canny and morphs
-        canny_low = cv2.getTrackbarPos('canny_low', 'BARS')
-        canny_high = 3 * canny_low
+        sigma = cv2.getTrackbarPos('sigma', 'BARS') / 100
+        v = np.median(blurred)
+        canny_low = int(max(0, (1 - sigma) * v))
+        canny_high = int(min(255, (1 + sigma) * v))
+        # canny_low = cv2.getTrackbarPos('canny_low', 'BARS')
+        # canny_high = 3 * canny_low
         # canny_high = max(canny_low, cv2.getTrackbarPos('canny_high', 'BARS'))
         edged = cv2.Canny(blurred, canny_low, canny_high)
         edged = cv2.dilate(edged, None, iterations=3)
         edged = cv2.erode(edged, None, iterations=2)
 
-        # Invert back
-        if cv2.getTrackbarPos('invert', 'BARS'):
-            image = cv2.bitwise_not(image)
-
         # Find contours
         _, contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         x0, y0 = image.shape[:2]
-        diag0 = (x0**2 + y0**2)**0.5
-        l = 400
+        diag0 = math.hypot(x0, y0)
+        # l = 400
+        l = max(0, cv2.getTrackbarPos('l', 'BARS'))*10
         width_mm, height_mm, diag_mm = getWHImage(l, 54.5, 42.3, 66.17)
         ratio = diag_mm / diag0
 
         for contour in contours:
             if cv2.contourArea(contour) < 200:
                 continue
+
+            # @rect :: tuple((center_x, center_y), (width, height), angle_deg)
+            # box :: np.array([[x y], [x y], [x y], [x y]]) -- ordered as (top_left, top_right, bottom_right, bottom_left)
+            # dA -- width [px]
+            # dB -- height [px]
+            # dD -- diagonal [px]
+            # dimA, dimB, dimD -- same as above [mm]
 
             rect = cv2.minAreaRect(contour)
             box = cv2.boxPoints(rect)
@@ -105,7 +119,7 @@ def main():
 
             alpha = math.atan2(dB, dA)
             dimD = dD * ratio
-            dimA = dimD * math.sin(alpha)
+            dimA = dimD * math.sin(alpha)  # Why not cos?
             dimB = dimD * math.cos(alpha)
 
             for x, y in box:
@@ -114,11 +128,11 @@ def main():
             cv2.drawContours(image, [box.astype('int')], -1, (0, 255, 0), 2)
             cv2.drawContours(image, [contour], -1, (0, 0, 255), 2)
 
-            cv2.putText(image, "{:.1f}px;{:.1f}mm".format(dA, dimA), (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.25, color=(255, 0, 0), thickness=1)
-            cv2.putText(image, "{:.1f}px;{:.1f}mm".format(dB, dimB), (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.25, color=(255, 0, 0), thickness=1)
+            put_text(image, (tltrX - 15, tltrY - 10), dimA)  # width
+            put_text(image, (trbrX + 10, trbrY), dimB)  # height
 
-        images_concatenated = cv2.hconcat([image, cv2.cvtColor(edged, cv2.COLOR_GRAY2BGR)])
-        cv2.imshow('MAIN', images_concatenated)
+        edged_bgr = cv2.cvtColor(edged, cv2.COLOR_GRAY2BGR)
+        cv2.imshow('MAIN', np.hstack([image, edged_bgr]))
 
         if cv2.waitKey(1) == 27:
             break
