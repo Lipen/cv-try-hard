7fa340957d5b8e2f69ec89fb5c5d963301a20c12
Lipen <lipen00@gmail.com>
2017-02-20 15:38:49 +0300

    Update shape matcher. Add canny/threshold/hybrid versions

--- lipen/match_shapes_hybrid.py
+++ lipen/match_shapes_hybrid.py
@@ -26,6 +26,7 @@ def init():
     cv2.namedWindow('Main')
     cv2.namedWindow('Bars')
 
+    cv2.createTrackbar('sigma', 'Bars', 33, 100, nothing)
     cv2.createTrackbar('threshold_min', 'Bars', 100, 255, nothing)
     cv2.createTrackbar('invert', 'Bars', 0, 1, nothing)
     cv2.createTrackbar('blur_type', 'Bars', 1, 3, nothing)
@@ -76,30 +77,61 @@ def main():
         threshold_min = cv2.getTrackbarPos('threshold_min', 'Bars')
         _, thresh = cv2.threshold(gray, threshold_min, 255, cv2.THRESH_BINARY)
 
+        # Edges
+        sigma = cv2.getTrackbarPos('sigma', 'Bars') / 100.
+        v = np.median(image)
+        canny_low = int(max(0, (1 - sigma) * v))
+        canny_high = int(min(255, (1 + sigma) * v))
+        edged = cv2.Canny(thresh, canny_low, canny_high)
+        edged = cv2.dilate(edged, None, iterations=3)
+        edged = cv2.erode(edged, None, iterations=2)
+
         # Find contours
-        _, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        _, contours, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        best = None
+        draw_contours = []  # (ret, contour)
 
         for contour in contours:
-            if cv2.contourArea(contour) < 500:
+            if cv2.contourArea(contour) < 300:
                 continue
 
             ret = cv2.matchShapes(contour, base_contour, 1, 0)
-            text = '{:.3f}'.format(ret)
+            if best is None or ret < best[0]:
+                best = (ret, contour)
 
+            draw_contours.append((ret, contour))
+
+        for ret, contour in draw_contours:
             M = cv2.moments(contour)
             text_pos = (int(M['m10'] / M['m00']),
                         int(M['m01'] / M['m00'])) if M['m00'] else (50, 50)
 
-            cv2.drawContours(image, [contour], -1, (0, 255, 0), 2)
-            cv2.putText(image, text, text_pos,
-                        cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8,
-                        color=(0, 0, 0), thickness=4)
-            cv2.putText(image, text, text_pos,
-                        cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8,
-                        color=(255, 255, 255), thickness=2)
+            mask = np.zeros(gray.shape, np.uint8)
+            cv2.drawContours(mask, [contour], -1, 255, -1)
+            color = cv2.mean(image, mask)
 
-        thresh_bgr = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
-        cv2.imshow("Main", np.hstack((image, thresh_bgr)))
+            if contour is best[1]:
+                text = 'Screw'
+                cv2.drawContours(image, [contour], -1, color, 2)
+                cv2.putText(image, text, text_pos,
+                            cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8,
+                            color=(0, 0, 0), thickness=4)
+                cv2.putText(image, text, text_pos,
+                            cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8,
+                            color=(224, 255, 224), thickness=2)
+            else:
+                text = '{:.3f}'.format(ret)
+                cv2.drawContours(image, [contour], -1, color, 2)
+                cv2.putText(image, text, text_pos,
+                            cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8,
+                            color=(0, 0, 0), thickness=4)
+                cv2.putText(image, text, text_pos,
+                            cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8,
+                            color=(255, 255, 255), thickness=2)
+
+        edged_bgr = cv2.cvtColor(edged, cv2.COLOR_GRAY2BGR)
+        cv2.imshow("Main", np.hstack((image, edged_bgr)))
 
         if cv2.waitKey(1) == 27:
             break
